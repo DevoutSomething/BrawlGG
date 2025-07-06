102c5b1834a893dc848e14a35d7cfc0fd5845bba
Archer L Fox <71037555+DevoutSomething@users.noreply.github.com>
2025-07-06 15:38:08 -0400

    Worked on data process, seperated 3v3 modes and showdown

--- backend/helpers/ProcessData.js
+++ backend/helpers/ProcessData.js
@@ -1,8 +1,5 @@
-const getBattlesFromPlayerID = async (playerID) => { 
-
+const getTopPlayerIDs = async () => { 
     let data;
-    let map = new Map();
-    totalBattles = 0;
     try {
         const response = await fetch(
         `${process.env.BRAWL_URL_RANKINGS}`,
@@ -22,22 +19,67 @@ const getBattlesFromPlayerID = async (playerID) => {
     }
 
     let items = data.items;
+    let playerIDs = [];
     for (let i = 0; i < items.length; i++) {
-      let curID = items[i].tag
-      await processUser(curID);
+    let curID = "23" + items[i].tag.slice(1);
+      playerIDs.push(curID);
     }
+    return playerIDs;
 }
 
 const processUser = async (playerTag) => { 
     //get the top 100 battles for the user and process them depending on the mode
+    const response = await fetch(
+        `${process.env.BRAWL_URL_PLAYERS}%${playerTag}/battlelog`,
+        {
+            headers: {
+                Accept: "application/json",
+                Authorization: `Bearer ${process.env.API_KEY_BRAWL}`,
+                "Cache-Control": "max-age=60",
+            },
+        }
+    );
+    if (!response.ok) {
+        throw new Error("user not found");
+    }
+    const data = await response.json();
+    if (!data) {
+        throw new Error("Unable to fetch data");
+    }
+    const items = data.items;
+    for (let i = 0; i < items.length; i++) {
+        const event = items[i].event;
+        if (event.mode === "brawlBall") {
+            processBattle3v3(items[i], playerTag);
+        }
+    }
 }
 
+const processBattleSoloShowdown = (battleJSON) => { 
+    const mode = "soloShowdown";
+    const map = battleJSON.event.map;
+    const players = battleJSON.battle.players;
+    for (let i = 0; i < players.length; i++) {
+        if (i <= 4){
+            brawlerWin(players[i].name, map, mode);
+        } else {
+            brawlerLoss(players[i].name, map, mode);
+        }
+    }
 
+}
+
+const brawlerWin = (brawlerName, map, mode) => { 
+    //update the database with the brawler win
+}
+
+const brawlerLoss = (brawlerName, map, mode) => { 
+    //update the database with the brawler loss
+}
 const processBattle3v3 = (battleJSON, playerTag) => { 
-    const items = battleJSON.items;
-    const battle = items.battle;
-    const teams = items.teams;
-    const event = battle.event;
+    const battle = battleJSON.battle;
+    const teams = battle.teams;
+    const event = battleJSON.event;
     const {mode, map} = event;
     let win = false;
     if (battle.result === "victory") { 
